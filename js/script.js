4dcfaad662478d666d11d4ef8d9bade30186e07a
Giorgio <1999giorgio.monne@mail.com>
2021-11-14 22:18:47 -1000

    fine parto :)

--- js/script.js
+++ js/script.js
@@ -23,43 +23,116 @@ const imageArray = [
     'img/02.jpg',
     'img/03.jpg',
     'img/04.jpg',
-    'img/05.jpg',
+    'img/05.jpg'
 ];
 
-// const imageLuoghi = [
-//     'img/01.jpg',
-//     'img/02.jpg',
-//     'img/03.jpg',
-//     'img/04.jpg',
-//     'img/05.jpg',
-// ];
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+];
 
-// const imageStati = [
-//     'img/01.jpg',
-//     'img/02.jpg',
-//     'img/03.jpg',
-//     'img/04.jpg',
-//     'img/05.jpg',
-// ];
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+];
 
 console.log(imageArray);
 
 // const itemsContainer = document.querySelector(".item-container");
 
 let itemsContent ='';
+let elementsContent ='';
 
 for(let i = 0; i < imageArray.length; i++){
     itemsContent += `
     <div class="item">
         <img src="${imageArray[i]}" alt="slider">
+        <div class ="descrizione">
+            <h1 class="title">${title[i]}</h1>
+            <p class="text">${text[i]}</p>
+        </div>
+    </div>`;
+
+    elementsContent += `
+    <div class="element">
+        <img src="${imageArray[i]}" alt="slider">
     </div>`;
 }
 
 console.log(itemsContent);
+console.log(elementsContent);
 
 const itemsContainer = document.querySelector('.items-container');
 itemsContainer.innerHTML = itemsContent;
 
-const item = document.querySelector('.item');
-console.log(item);
-item.className = "item active";
+const itemsContainerDestra = document.querySelector('.items-container-destra');
+itemsContainerDestra.innerHTML += elementsContent;
+
+
+// const item = document.querySelector('.item');
+// console.log(item);
+// item.className = "item active";
+
+// versione diversa cambio active
+
+
+const item = document.getElementsByClassName('item');
+console.log(item[0]);
+let activeItem = 0;
+item[activeItem].classList.add('active');
+
+const element = document.getElementsByClassName('element');
+console.log(element[0]);
+element[activeItem].classList.add('active');
+
+
+// active frecce
+// const circle = document.getElementsByClassName('pallino');
+
+
+const prev = document.querySelector('.precedente');
+const next = document.querySelector('.successiva');
+
+next.addEventListener("click", function(){
+
+    item[activeItem].classList.remove('active');
+    element[activeItem].classList.remove('active');
+
+    if(activeItem < imageArray.length-1){
+        // circle[activeItem].classList.remove('active');
+        activeItem++;
+
+        item[activeItem].classList.add('active');
+        element[activeItem].classList.add('active');
+        // circle[activeItem].classList.add('active');
+    }else{
+        activeItem = 0;
+        item[activeItem].classList.add('active');
+        element[activeItem].classList.add('active');
+    }
+});
+
+prev.addEventListener("click", function(){
+
+    item[activeItem].classList.remove('active');
+    element[activeItem].classList.remove('active');
+
+    if(activeItem > 0){
+       // circle[activeItem].classList.remove('active');
+        activeItem--;
+
+        item[activeItem].classList.add('active');
+        element[activeItem].classList.add('active');
+        // circle[activeItem].classList.add('active');
+    }else if(activeItem == 0){
+        activeItem = imageArray.length-1;
+        item[activeItem].classList.add('active');
+        element[activeItem].classList.add('active');
+    }
+});
